a651d9eb5e0c763ec63d8fc364ad56793343eeb1
TheCanadian0417 <hhancharuk@gmail.com>
2023-10-22 19:18:38 -0400

    Fixed

--- src/main/java/net/thecanadian0417/extraarmor/datagen/DataGenerators.java
+++ src/main/java/net/thecanadian0417/extraarmor/datagen/DataGenerators.java
@@ -1,10 +1,12 @@
 package net.thecanadian0417.extraarmor.datagen;
 
+import com.google.common.eventbus.Subscribe;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.PackOutput;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.data.event.GatherDataEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.thecanadian0417.extraarmor.ExtraArmor;
 
@@ -12,6 +14,7 @@ import java.util.concurrent.CompletableFuture;
 
 @Mod.EventBusSubscriber(modid = ExtraArmor.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class DataGenerators {
+    @SubscribeEvent
     public static void gatherData(GatherDataEvent event) {
         DataGenerator generator = event.getGenerator();
         PackOutput packOutput = generator.getPackOutput();
