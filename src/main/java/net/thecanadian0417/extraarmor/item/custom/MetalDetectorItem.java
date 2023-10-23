a32a0d00ae8c7264e061b8ca24417a14cfc65ac6
TheCanadian0417 <hhancharuk@gmail.com>
2023-10-22 21:25:08 -0400

    Added Metal Detector Tooltip

--- src/main/java/net/thecanadian0417/extraarmor/item/custom/MetalDetectorItem.java
+++ src/main/java/net/thecanadian0417/extraarmor/item/custom/MetalDetectorItem.java
@@ -1,16 +1,23 @@
 package net.thecanadian0417.extraarmor.item.custom;
 
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.resources.language.I18n;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import net.thecanadian0417.extraarmor.util.ModTags;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class MetalDetectorItem extends Item {
     public MetalDetectorItem(Properties pProperties)
@@ -47,6 +54,17 @@ public class MetalDetectorItem extends Item {
 
 
         return InteractionResult.SUCCESS;
+    }
+
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced) {
+        if (Screen.hasShiftDown()) {
+            pTooltipComponents.add(Component.translatable("tooltip.extra_armor.metal_detector.tooltip.shift"));
+        } else {
+            pTooltipComponents.add(Component.translatable("tooltip.extra_armor.metal_detector.tooltip"));
+        }
+
+        super.appendHoverText(pStack, pLevel, pTooltipComponents, pIsAdvanced);
     }
 
     private void outputValuableCoordinates(BlockPos blockPos, Player player, Block block) {
